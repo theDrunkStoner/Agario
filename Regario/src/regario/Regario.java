099eb4b91f2c89547d4af6e6f80904e9277219f1
Minetoblend <Minetoblend@gmail.com>
2016-05-12 09:59:04 +0200

    basic window settings and canvas test

--- Regario/src/regario/Regario.java
+++ Regario/src/regario/Regario.java
@@ -5,40 +5,56 @@
  */
 package regario;
 
+import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
  * @author marvin
  */
 public class Regario extends Application {
-    
+
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+    public void start(Stage stage) {
+        stage.initStyle(StageStyle.TRANSPARENT);
+
+        StackPane stackPane = new StackPane();
+
+        stackPane.setStyle(
+                "-fx-background-color: rgba(0,0,0,0.05);"
+                + "-fx-background-insets: 50;"
+        );
+        final Canvas canvas = new Canvas(450, 450);
+        stackPane.getChildren().add(canvas);
+        
+        new AnimationTimer() {
+
             @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+            public void handle(long l) {
+                double size = Math.cos(l/100000000f)*10 + 30;
+                GraphicsContext g = canvas.getGraphicsContext2D();
+                g.clearRect(0, 0, 450, 450);
+                g.fillOval(225-size, 225-size, size*2, size*2);
             }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        }.start();
+
+        Scene scene = new Scene(stackPane, 450, 450);
+        scene.setFill(Color.TRANSPARENT);
+
+        stage.setScene(scene);
+
+        stage.show();
     }
 
     /**
@@ -47,5 +63,5 @@ public class Regario extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
